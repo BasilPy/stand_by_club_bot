11c5890e1198fe199f9959cdb21f4815ad9bc242
Basil Sutormin <sudobasil@gmail.com>
2023-02-05 13:58:16 +0600

    refactor creating bot instance

---
+++ standby_aiogram/create_bot.py
@@ -0,0 +1,6 @@
+from aiogram import Bot, Dispatcher, executor, types
+
+# TOKEN = "634110425:AAFaoF8R730O5JxQdwpw3wfhl39-f3I0xTI"
+TOKEN = "5848810697:AAHsWpf7zSDb0tuEzQ3lTx1-6dYQq3rwPcI"
+bot = Bot(token=TOKEN)
+dispatcher = Dispatcher(bot)
